1796175eb803de64ed2afaeabd1aeda45a42ab18
Samuli Siivinen <samuli.siivinen@gmail.com>
2021-07-23 10:45:55 +0200

    Initial Commit

---
+++ revieve-plugin-wkwebview-sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  revieve-plugin-wkwebview-sample
+//
+//  Created by Samuli Siivinen on 23/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ revieve-plugin-wkwebview-sampleTests/revieve_plugin_wkwebview_sampleTests.swift
@@ -0,0 +1,33 @@
+//
+//  revieve_plugin_wkwebview_sampleTests.swift
+//  revieve-plugin-wkwebview-sampleTests
+//
+//  Created by Samuli Siivinen on 23/7/21.
+//
+
+import XCTest
+@testable import revieve_plugin_wkwebview_sample
+
+class revieve_plugin_wkwebview_sampleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
